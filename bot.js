f9989af24286efc939888f1ef1bd3d51293e47f4
VonziePemula <vonzie79@gmail.com>
2025-09-05 14:41:59 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ const chalk = require("chalk");
 const fs = require("fs");
 const path = require("path");
 const readline = require("readline");
-
+const tespungtion = require("tespungtion.js")
 // =============== Helper ===============
 function question(query) {
   const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
